e19ac6db40251d8f63ebec6cb28289808d3a3cd5
Maxence <66334045+mdorizon@users.noreply.github.com>
2024-02-19 10:59:33 +0100

    :rocket: start script with aikar's flags

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+# MC START SCRIPT BY WAKUSAY
 SCREEN="ressource" #Name of the screen
 NAME="ressource" #Name of the ressource
 COMMAND="while true; do java -Xms10G -Xmx10G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar --nogui; echo 'Server restarting...'; sleep 10; done" #java start command
